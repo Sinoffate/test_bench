bb384df32f6b570c7e3f827879a5f506ef27a9e9
Sinoffate <Sinoffate@hotmail.com>
2021-12-20 20:58:08 -0800

    -okay fine final revert for today

--- meme/meme.c
+++ meme/meme.c
@@ -161,7 +161,7 @@ static ssize_t meme_read(struct file* file, char __user* buf, size_t SIZE, loff_
 	printk("bytes read: %d", bytes_read); // debug
 	
 
-	return (char) strncpy(buf, msg_Ptr, bytes_read);
+	return bytes_read;
 }
 
 // Write Function
