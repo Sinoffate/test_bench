3fcb76a7ccc4578bd33f8b8c9a7b07a52a650946
Sinoffate <Sinoffate@hotmail.com>
2021-12-20 20:56:55 -0800

    -minor fix via typecast

--- meme/meme.c
+++ meme/meme.c
@@ -161,7 +161,7 @@ static ssize_t meme_read(struct file* file, char __user* buf, size_t SIZE, loff_
 	printk("bytes read: %d", bytes_read); // debug
 	
 
-	return strncpy(buf, msg_Ptr, bytes_read);
+	return (char) strncpy(buf, msg_Ptr, bytes_read);
 }
 
 // Write Function
