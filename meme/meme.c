efdd17a8bbb89e9cb3cd76b90a28f1b88aeafb9a
Sinoffate <Sinoffate@hotmail.com>
2021-12-22 14:21:35 -0800

    -attempt03

--- meme/meme.c
+++ meme/meme.c
@@ -178,7 +178,7 @@ static ssize_t meme_write(struct file* file, const char __user* buf, size_t SIZE
 
 static int meme_increment(struct meme_increment_t __user *arg)
 {
-	int meme = *arg;
+	uint64_t meme = *arg;
 	meme++;
 	
 	return meme;
