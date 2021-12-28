70533d277354b9814fc2db4abf043f16eaec8cab
Sinoffate <Sinoffate@hotmail.com>
2021-12-27 21:32:03 -0800

    -One more time for tonight

--- meme/meme.c
+++ meme/meme.c
@@ -179,7 +179,9 @@ static ssize_t meme_write(struct file* file, const char __user* buf, size_t SIZE
 static int meme_increment(struct meme_increment_t __user *arg)
 {
 	int meme = 0;
-	arg->meme;
+	arg->target;
+
+	target = meme;
 
 	return meme++;
 }
