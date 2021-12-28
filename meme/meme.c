4933a25629835898be34acea8522297177372565
Sinoffate <Sinoffate@hotmail.com>
2021-12-27 21:18:01 -0800

    -changed meme_increment function

--- meme/meme.c
+++ meme/meme.c
@@ -179,7 +179,8 @@ static ssize_t meme_write(struct file* file, const char __user* buf, size_t SIZE
 static int meme_increment(struct meme_increment_t __user *arg)
 {
 	int target;
-	*arg->target;
+	int* arg = target;
+	
 
 	return target++;
 }
